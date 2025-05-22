5180ec0bb366cd0736493b192da983a79facb148
JordanAzo <jgodinez50085@ufide.ac.cr>
2025-05-21 19:14:30 -0600

    Commit 1
    Mi primer commit

---
+++ Tienda_Jordan/src/main/java/Tienda/Web/TiendaJordanApplication.java
@@ -0,0 +1,13 @@
+package Tienda.Web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaJordanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaJordanApplication.class, args);
+	}
+
+}
