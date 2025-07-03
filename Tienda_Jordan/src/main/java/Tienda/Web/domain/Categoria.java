afb9cd107a58d3e42a0655bc90f8fc669f6575a2
JordanAzo <jgodinez50085@ufide.ac.cr>
2025-07-02 21:32:22 -0600

    Semana 8

--- Tienda_Jordan/src/main/java/Tienda/Web/domain/Categoria.java
+++ Tienda_Jordan/src/main/java/Tienda/Web/domain/Categoria.java
@@ -5,31 +5,47 @@
 
 package Tienda.Web.domain;
 
-
 import jakarta.persistence.*;
 import java.io.Serializable;
+import java.util.List;
 import lombok.Data;
 
 @Data
 @Entity
-@Table(name="categoria")
+@Table(name = "categoria")
 public class Categoria implements Serializable {
     
     private static final long serialVersionUID = 1L;
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="id_categoria")
+    @Column(name = "id_categoria")
     private Long idCategoria;
+    
     private String descripcion;
     private String rutaImagen;
     private boolean activo;
-
+    
+    // Relación One-to-Many con Producto
+    @OneToMany(mappedBy = "categoria", 
+               cascade = CascadeType.ALL, 
+               fetch = FetchType.LAZY)
+    private List<Producto> productos;
+    
+    // Constructor vacío
     public Categoria() {
     }
-
-    public Categoria(String categoria, boolean activo) {
-        this.descripcion = categoria;
+    
+    // Constructor con descripción
+    public Categoria(String descripcion, boolean activo) {
+        this.descripcion = descripcion;
+        this.activo = activo;
+    }
+    
+    // Constructor completo
+    public Categoria(String descripcion, String rutaImagen, boolean activo) {
+        this.descripcion = descripcion;
+        this.rutaImagen = rutaImagen;
         this.activo = activo;
     }
 }
