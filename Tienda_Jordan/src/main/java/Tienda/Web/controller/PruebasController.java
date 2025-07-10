71d9929e8289e1a1f33b47813a7510e134f38138
JordanAzo <jgodinez50085@ufide.ac.cr>
2025-07-09 23:34:31 -0600

    semana9

--- Tienda_Jordan/src/main/java/Tienda/Web/controller/PruebasController.java
+++ Tienda_Jordan/src/main/java/Tienda/Web/controller/PruebasController.java
@@ -5,6 +5,12 @@
 
 package Tienda.Web.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
 import Tienda.Web.domain.Categoria;
 import Tienda.Web.Service.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,5 +47,33 @@ public class PruebasController {
         model.addAttribute("totalProductos", productos.size());
         model.addAttribute("categorias", categorias);
         return "/pruebas/listado";
+    }
+    
+    @GetMapping("/listado2")
+    public String listado2(Model model){
+        var productos = productoService.getProductos(false);
+        model.addAttribute("productos", productos);
+        return "/pruebas/listado2";
+    }
+    
+    @PostMapping("/query1")
+    public String consultaQuery1(@RequestParam(value = "precioInf")double precioInf, 
+            @RequestParam(value = "precioSup")double precioSup, Model model){
+        var productos = productoService.findByPrecioBetweenOrderByDescripcion(precioInf, precioSup);
+        model.addAttribute("productos", productos);
+        model.addAttribute("precioInf", precioInf);
+        model.addAttribute("precioSup", precioSup);
+        return "/consulta/listado";
+    }
+    
+    @PostMapping("/query2")
+    public String consultaQuery2(@RequestParam(value = "precioInf") double precioInf, 
+            @RequestParam(value = "precioSup") double precioSup, Model model) {
+        var productos = productoService.metodoJPQL(precioInf, precioSup);
+        model.addAttribute("productos", productos);
+        model.addAttribute("totalProductos", productos.size());
+        model.addAttribute("precioInf", precioInf);
+        model.addAttribute("precioSup", precioSup);
+        return "/pruebas/listado2";
     }
 }
--- Tienda_Jordan/src/main/java/Tienda/Web/dao/ProductoDao.java
+++ Tienda_Jordan/src/main/java/Tienda/Web/dao/ProductoDao.java
@@ -5,9 +5,16 @@
 
 package Tienda.Web.dao;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import Tienda.Web.domain.Producto;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductoDao extends JpaRepository <Producto, Long> {
     
+    public List<Producto> findByPrecioBetweenOrderByDescripcion(double precioInf, double precioSup);
+    
+    @Query(value="SELECT a FROM Producto a where a.precio BETWEEN :precioInf AND :precioSup ORDER BY a.descripcion ASC")
+    public List<Producto> metodoJPQL(@Param("precioInf")double precioInf, @Param("precioSup")double precioSup);
 }
